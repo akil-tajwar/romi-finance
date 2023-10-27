7653cdeaaa8005fa61bd663cb7d7136c77de39f6
Akil Tajwar <akiltajwar@gmail.com>
2023-10-27 21:25:11 +0600

    navbar complete

--- src/Pages/Shared/Navbar.jsx
+++ src/Pages/Shared/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { CgProfile } from 'react-icons/cg';
+import { LuWallet2 } from 'react-icons/lu';
+import { BsThreeDotsVertical } from 'react-icons/bs';
+import { GiHamburgerMenu } from 'react-icons/gi';
 
 const Navbar = () => {
     const menu = (
@@ -17,35 +19,43 @@ const Navbar = () => {
     )
     return (
         <div className='fixed w-full z-10'>
-            <div className='backdrop-blur-sm shadow lg:w-3/4 mx-auto rounded-lg'>
-                <div className='flex justify-between items-center'>
-                    <div className='flex items-center'>
-                        {/* <div className="dropdown lg:hidden">
-                            <label tabIndex={0} className="btn btn-ghost btn-circle">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
-                            </label>
-                            <ul tabIndex={0} className="mt-3 z-[1] py-3 px-5 shadow menu menu-sm dropdown-content text-black bg-base-100 rounded-box w-52">
-                                {menu}
-                            </ul>
-                        </div> */}
-                        <div className="drawer lg:hidden block">
-                            <input id="my-drawer" type="checkbox" className="drawer-toggle" />
-                            <div className="drawer-content">
-                                <label htmlFor="my-drawer" className="btn drawer-button">
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
-                                </label>
-                            </div>
-                            <div className="drawer-side">
-                                <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
-                                <ul className="menu w-52 min-h-full bg-base-200 text-base-content">
-                                    {menu}
-                                </ul>
+            <div className='bg-[#191919] shadow py-3 lg:py-0'>
+                <div className='w-[97%] mx-auto'>
+                    <div className='flex justify-between items-center'>
+                        <div className='flex items-center'>
+                            <div className='flex justify-center items-center'>
+                                <div className="drawer lg:hidden block">
+                                    <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+                                    <div className="drawer-content">
+                                        <label htmlFor="my-drawer" className="drawer-button">
+                                            <GiHamburgerMenu/>
+                                        </label>
+                                    </div>
+                                    <div className="drawer-side">
+                                        <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
+                                        <ul className="menu w-64 min-h-full bg-base-200 text-base-content">
+                                            {menu}
+                                        </ul>
+                                    </div>
+                                </div>
+                                <img className='lg:w-14 w-8' src="New Logo.png" alt="" />
+                                <p className="lg:text-2xl text-sm font-semibold">Romi.Finance</p>
                             </div>
                         </div>
-                        <p className="text-2xl font-semibold">Romi.Finance</p>
+                        <div className="hidden lg:block">{menu}</div>
+                        <div className='flex gap-4'>
+                            <button className='bg-[#3EB489] hover:bg-[#339672] px-4 py-1 rounded'>Trade</button>
+                            <div className='border border-[#3E3E3E] cursor-pointer hover:bg-[#2e2e2e] flex gap-2 items-center px-4 py-1 rounded'>
+                                <LuWallet2 />
+                                <p className='hidden lg:block'>Connect Wallet</p>
+                                <p className='lg:hidden block'>Connect</p>
+                            </div>
+                            <div className='border border-[#3E3E3E] cursor-pointer hover:bg-[#2e2e2e] flex gap-2 items-center px-2 py-1 rounded'>
+                                <img className='border-r border-[#3E3E3E] pr-3 w-8' src="Ethereum.svg" alt="" />
+                                <BsThreeDotsVertical />
+                            </div>
+                        </div>
                     </div>
-                    <div className="hidden lg:block">{menu}</div>
-                    <div>button</div>
                 </div>
             </div>
         </div>
